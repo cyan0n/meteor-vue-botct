63d41a3b76d1d858a3bab766fa1df8a54bf3d38e
Thomas Cipriani <thomas.cipriani@esimple.it>
2019-04-18 18:32:02 +0200

    checkpoint

--- client/App.vue
+++ client/App.vue
@@ -1,13 +1,60 @@
 <template>
 	<div class="grid">
 		<router-view class="content"></router-view>
+		<role-token :role="role"></role-token>
 	</div>
 </template>
 
 <script>
+import RoleToken from '/imports/view/RoleToken.vue';
+
+
 
 export default {
-	components: {  }
+	data() {
+		return {
+			role: {
+				type: "townsfolk",
+				edition: "troubles_brewing",
+				label: "washerwoman",
+				name: {
+					en: "Washerwoman",
+					it: "Lavandaia",
+				},
+				icon: 'Washerwoman_icon.png',
+				tokens: [
+					{
+						label: "wrong",
+						name: {
+							en: "Wrong"
+						},
+						quantity: 1
+					},
+					{
+						label: "townsfolk",
+						name: {
+							en: "Townsfolk"
+						},
+						quantity: 1
+					},
+				],
+				wake: 1,
+				description: {
+					en: "You start knowing that 1 of 2 players is a particular Townsfolk",
+					it: "",
+				},
+				flavour: {
+					en: "Bloodstains on a dinner jacket? No. This is cooking sherry. How careless.",
+					it: "",
+				},
+				almanac: {
+					en: "",
+					it: "",
+				}
+			}
+		}
+	},
+	components: { RoleToken }
 }
 </script>
 
---
+++ imports/view/RoleToken.vue
@@ -0,0 +1,50 @@
+<template>
+	<div class="role-token">
+		<h4>{{ role.name.en }}</h4>
+		<svg viewBox="0 0 130 130">
+			<path d="m20,70c0,-27.62431 22.37569,-50 50,-50c27.62431,0 50,22.37569 50,50c0,27.62431 -22.37569,50 -50,50c-27.62431,0 -50,-22.37569 -50,-50z" fill="black" id="circle" stroke-dasharray="null" stroke-linecap="null" stroke-linejoin="null" stroke-width="null" transform="rotate(-90 70 70)"/>
+			<text width="500" text-anchor="middle">
+				<textPath xlink:href="#circle" startOffset="50%">
+				Dangerous Curves Ahead
+				</textPath>
+			</text>
+		</svg>
+		<img :src="icon">
+		<p>{{ role.description.en }}</p>
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			//role: null,
+		}
+	},
+	computed: {
+		icon() {
+			return '/images/icons/' + this.role.icon;
+		}
+	},
+	props: [ 'role' ],
+}
+</script>
+
+<style lang="scss" scoped>
+	.role-token {
+		border: 6px solid #00b89c;
+		background: white;
+		position: absolute;
+		border-radius: 100px;
+		overflow: hidden;
+		//height: 200px;
+		//width: 200px;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+	svg {
+		width: 100px;
+	}
+</style>
+
