d8ef38e42a4b7e751e8779647b1a972d3e4b12d2
Cyan0n <cyan0n.dev@gmail.com>
2019-04-23 13:56:12 +0200

    Basic preperation page

---
+++ imports/views/pages/Prep.vue
@@ -0,0 +1,22 @@
+<template>
+	<div>
+	<!-- Choose number of players -->
+	<select>
+		<option :value="index" v-for="(item, index) in player_numbers"></option>
+	</select>
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			player_numbers: Meteor.settings.players,
+		}
+	},
+}
+</script>
+
+<style>
+
+</style>
